ce393781ea63c0e02c5787f38f38fa7204c66927
Austin <austinquachh@gmail.com>
2014-06-14 20:51:38 -0700

    Revert "Removed unnecessary settings"
    This reverts commit 3a227e63d5548ea07a34e505d917f16c06a317e0.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -167,7 +167,7 @@ public class Settings extends PreferenceActivity
             R.id.interface_menus,
             R.id.interface_notification_panel,
             R.id.active_display,
-            //R.id.interface_more,
+            R.id.interface_more,
             R.id.button_settings
     };
 
@@ -632,10 +632,10 @@ public class Settings extends PreferenceActivity
                 if (!showDev) {
                     target.remove(i);
                 }
-            /*} else if (id == R.id.performance_controls) {
+            } else if (id == R.id.performance_controls) {
                 if (!showDev) {
                     target.remove(i);
-                }*/
+                }
             } else if (id == R.id.account_add) {
                 if (um.hasUserRestriction(UserManager.DISALLOW_MODIFY_ACCOUNTS)) {
                     target.remove(i);
